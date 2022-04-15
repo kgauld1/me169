72691a94e5e7562b4989bc25faecae3f6ff39866
Maddix9 <100259394+Maddix9@users.noreply.github.com>
2022-04-15 12:55:26 -0700

    Update wheelcontrol_skeleton.py

--- wheelcontrol_skeleton.py
+++ wheelcontrol_skeleton.py
@@ -101,8 +101,8 @@ def callback_timer(event):
 
     # Generate motor commands (convert wheel speed to PWM)
     lam2 = .05/.1
-    ldesv = lwcomm + lam2*((lint-pleft))
-    rdesv = rwcomm + lam2*((rint-pright))
+    ldesv = lwcomm - lam2*((lint-pleft))
+    rdesv = rwcomm - lam2*((rint-pright))
     print([lint, rint])
     print([lam2*((lint-pleft)),lam2*((rint-pright))])
     
