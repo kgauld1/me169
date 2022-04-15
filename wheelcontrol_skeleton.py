63a3aa6bb52c285b5e03dcc017c4f742f11d71f0
Maddix9 <100259394+Maddix9@users.noreply.github.com>
2022-04-15 13:08:57 -0700

    Update wheelcontrol_skeleton.py

--- wheelcontrol_skeleton.py
+++ wheelcontrol_skeleton.py
@@ -103,16 +103,14 @@ def callback_timer(event):
     lam2 = .05/.01
     ldesv = lwcomm + lam2*((lint-pleft))
     rdesv = rwcomm + lam2*((rint-pright))
-    print([lint, rint])
-    print([pleft,pright])
-    print([lam2*((lint-pleft)),lam2*((rint-pright))])
+
     
     lpwm = ((abs(ldesv)*9) + 30) * math.copysign(1, ldesv)
     rpwm = ((abs(rdesv)*9) + 30) * math.copysign(1, rdesv)
     if lpwm > 255:
         lpwm = 255
-    if rpwm > 255:
-        rpwm = 255
+    if rpwm > 254:
+        rpwm = 254
     # Send wheel commands.
     driver.left(lpwm)
     driver.right(rpwm)
