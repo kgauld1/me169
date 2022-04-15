c4cb269316bef099540e99b93ea4fc7592f41473
Maddix9 <100259394+Maddix9@users.noreply.github.com>
2022-04-14 22:14:16 -0700

    Update wheelcontrol_skeleton.py

--- wheelcontrol_skeleton.py
+++ wheelcontrol_skeleton.py
@@ -66,8 +66,8 @@ def callback_timer(event):
     pleft = (encoder.leftencoder() / 45) * (2*math.pi / 16)
     pright = (encoder.rightencoder() / 45) * (2*math.pi / 16)
     const = 0
-    vleft = (const*lvel) + ((1-const)*(pleft-lpos)/((now-old).to_sec))
-    vright = (const*rvel) + ((1-const)*(pright-rpos)/((now-old).to_sec))
+    vleft = (const*lvel) + ((1-const)*(pleft-lpos)/(now.to_sec-old.to_sec))
+    vright = (const*rvel) + ((1-const)*(pright-rpos)/(now.to_sec-old.to_sec))
     # Add feedback?
     
 
